e3877ef4147d850705269e4d2ffcb8115f74a322
Sakura <brynhild@pku.edu.cn>
2022-12-12 21:50:29 +0800

    增加了matplotlib中文化的一般解决方法

--- 13-miniconda.sh
+++ 13-miniconda.sh
@@ -18,6 +18,7 @@ custom_channels:
     pytorch: https://mirrors.pku.edu.cn/anaconda/cloud' > ~/.condarc
 conda clean -i
 
+
 ## 删除环境
 # conda remove -n 环境名 --all
 ## conda批量导出包含环境中所有组件的requirements.txt文件
@@ -25,7 +26,55 @@ conda clean -i
 ## conda批量安装requirements.txt文件中包含的组件依赖
 # conda install --yes --file requirements.txt
 
-# 如果要安装cv2，conda install opencv(而不是opencv-python)
+
+## matplotlib中文化
+# from matplotlib.font_manager import FontProperties
+# import os
+# import matplotlib.pyplot as plt
+# import numpy as np
+# # 单项设置
+# def get_font(index=0):
+#     with os.popen('fc-list :lang=zh') as f:
+#         fonts = f.readlines()
+#         font_path = fonts[index].split(":")[0]
+#         return FontProperties(fname=font_path
+# def test():
+#     x = np.linspace(-1, 1, 100)
+#     y = np.sin(x)
+#     plt.plot(x, y)
+#     font = get_font()
+#     plt.xlabel("自变量", font=font)
+#     plt.ylabel("因变量$(^\circ)$", font=font)
+#     plt.show()
+# # 全局设置
+# def get_font_name(index=0):
+#     with os.popen('fc-list :lang=zh') as f:
+#         fonts = f.readlines()
+#         font_name = fonts[index].split(": ")[1].split(",")[0]
+#         return font_name
+# def temp():
+#     x = np.linspace(-1, 1, 100)
+#     y = np.sin(x)
+#     plt.plot(x, y)
+#     print(get_font_name())
+#     plt.rcParams['font.sans-serif'] = get_font_name()
+#     plt.rcParams['axes.unicode_minus'] = False
+#     plt.xlabel("自变量")
+#     plt.ylabel("因变量$(^\circ)$")
+#     plt.show()
+# # test()
+# temp()
+## 其他操作系统
+# import platform
+# plt_font = 'SimHei'  # windows上显示中文标签
+# if platform.system() == 'Darwin':
+#     plt_font = ['Heiti TC']  # 用来在macos上正常显示中文标签
+# plt.rcParams['font.sans-serif'] = plt_font
+# plt.rcParams['axes.unicode_minus'] = False  # 正常显示负号
+
+
+## 如果要安装cv2，conda install opencv(而不是opencv-python)
+
 
 ## jupyter notebook的配置
 
