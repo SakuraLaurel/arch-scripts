2e54f45e3f1c43e5e2b548c710b80ab77dab0657
Sakura <1700012467@pku.edu.cn>
2022-12-21 22:01:32 +0800

    更新了ssh的X11选项

--- 02-ssh.sh
+++ 02-ssh.sh
@@ -3,6 +3,10 @@ sudo pacman -S openssh
 sudo systemctl enable sshd.service
 sudo systemctl start sshd.service
 
+# 允许远程连接时添加-X选项，显示图形化界面。后来我才知道，后缀名为.service时可以不加。
+sudo sed -i "s|#X11Forwarding no|X11Forwarding yes|" /etc/ssh/sshd_config
+sudo systemctl restart sshd
+
 # 修改端口为19810，提高安全性。也可以不修改。
 sudo sed -i "s|#Port 22|Port 19810|" /etc/ssh/sshd_config
 sudo systemctl restart sshd.service
