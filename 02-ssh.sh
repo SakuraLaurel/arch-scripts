dcc8950c0874473b76dc8ff309ea7e2ffe43c73e
Sakura <1700012467@pku.edu.cn>
2022-12-21 22:30:15 +0800

    排除了一些matlab的问题

--- 02-ssh.sh
+++ 02-ssh.sh
@@ -10,3 +10,13 @@ sudo systemctl restart sshd
 # 修改端口为19810，提高安全性。也可以不修改。
 sudo sed -i "s|#Port 22|Port 19810|" /etc/ssh/sshd_config
 sudo systemctl restart sshd.service
+
+# 使用Matlab过程中遇到的问题：
+# 主要排查方式是运行`matlab/bin/glnxa64/MATLABWindow`，处理报错
+# 第一条报错：...matlab/bin/glnxa64/libtiff.so.5: version `LIBTIFF_4.0' not found (required by /usr/lib/libgdk_pixbuf-2.0.so.0)
+# 查了一下github等，认为是libtiff不兼容。处理方法是删掉（后面详述）libtiff.so.*
+# 第二条报错：...matlab/bin/glnxa64/MATLABWindow: symbol lookup error: /usr/lib/libharfbuzz.so.0: undefined symbol: FT_Get_Transform
+# 查了一下arch wiki，认定“FT”不是傅里叶变换，而是freetype2包的freetype的意思，同样出现了不兼容问题。删去对应的链接库。
+# 删去方法：在glnxa64下新建文件夹exclude，然后
+# mv libtiff.so.* exclude/
+# mv libfreetype.so* exclude/
