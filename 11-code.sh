640b230304e83114853ab511ce2e3d2331cddfaa
樱花桂冠 <1700012467@pku.edu.cn>
2023-11-23 23:36:21 +0800

    Update 11-code.sh

--- 11-code.sh
+++ 11-code.sh
@@ -1,8 +1,19 @@
-sudo pacman -S lsof base-devel
-# git clone -> makepkg -> pacman -U -> rm是AUR包基本的操作
-git clone https://aur.archlinux.org/visual-studio-code-bin.git
-cd visual-studio-code-bin
-makepkg
-sudo pacman -U *.zst
-cd ..
-rm -r visual-studio-code-bin
+# 已经不再推荐手动安装，推荐使用paru
+
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+# 然后
+
+paru visual-studio-code-bin
+
+# sudo pacman -S lsof base-devel
+# # git clone -> makepkg -> pacman -U -> rm是AUR包基本的操作
+# git clone https://aur.archlinux.org/visual-studio-code-bin.git
+# cd visual-studio-code-bin
+# makepkg
+# sudo pacman -U *.zst
+# cd ..
+# rm -r visual-studio-code-bin
