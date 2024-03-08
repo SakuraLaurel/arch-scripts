9d28bc4ee006d3b3ae88791ac7f32ec66f1a1c0e
樱花桂冠 <1700012467@pku.edu.cn>
2024-03-08 21:48:17 +0800

    Update 09-fcitx5.sh

--- 09-fcitx5.sh
+++ 09-fcitx5.sh
@@ -1,6 +1,8 @@
 sudo pacman -S fcitx5-im fcitx5-chinese-addons fcitx5-mozc fcitx5-pinyin-zhwiki
-echo 'GTK_IM_MODULE=fcitx
-QT_IM_MODULE=fcitx
-XMODIFIERS=@im=fcitx
+# 备注：kde5版本除了下列设置外，还推荐设置
+# GTK_IM_MODULE=fcitx
+# QT_IM_MODULE=fcitx
+# 但是kde6不推荐设置，因为GUI服务器换为了wayland
+echo 'XMODIFIERS=@im=fcitx
 SDL_IM_MODULE=fcitx
 GLFW_IM_MODULE=ibus' | sudo tee -a /etc/environment
