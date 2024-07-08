67f33c898ff122bcd7f0a81555c5f6842633a6bc
樱花桂冠 <1700012467@pku.edu.cn>
2024-07-08 20:39:15 +0800

    挂载windows系统盘

--- 06-disk.sh
+++ 06-disk.sh
@@ -6,3 +6,5 @@ sudo blkid
 disk_id="016AD11517FCF203"
 disk_dir="/home/sakura/Lys"
 echo "UUID=$disk_id $disk_dir ntfs defaults,uid=1000,gid=1000,umask=022 0 0" | sudo tee -a /etc/fstab
+# 挂载windows系统盘时可能会提示无法挂载，可以使用以下命令
+# sudo ntfs-3g -o remove_hiberfile /dev/source target
