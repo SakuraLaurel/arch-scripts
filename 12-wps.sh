70d9d026cd1b9a42f72263da734640fc81fa6448
樱花桂冠 <1700012467@pku.edu.cn>
2024-08-22 10:39:41 +0800

    更新了pdf所需的libtiff5

--- 12-wps.sh
+++ 12-wps.sh
@@ -1,22 +1,24 @@
-git clone https://aur.archlinux.org/wps-office.git
-cd wps-office
-makepkg
-sudo pacman -U *.zst
-cd ..
-rm -r wps-office
+paru wps-office ttf-wps-fonts wps-office-mui-zh-cn libtiff5
 
-# 安装专用字体，否则可能导致ppt乱码
-git clone https://aur.archlinux.org/ttf-wps-fonts.git
-cd ttf-wps-fonts
-makepkg
-sudo pacman -U *.zst
-cd ..
-rm -r ttf-wps-fonts
+# git clone https://aur.archlinux.org/wps-office.git
+# cd wps-office
+# makepkg
+# sudo pacman -U *.zst
+# cd ..
+# rm -r wps-office
 
-# 如果需要中文化，注意只需要安装wps-office-mui-zh-cn
-git clone https://aur.archlinux.org/wps-office-cn.git
-cd wps-office-cn
-makepkg
-sudo pacman -U wps-office-mui-zh-cn*.zst
-cd ..
-rm -r wps-office-cn
+# # 安装专用字体，否则可能导致ppt乱码
+# git clone https://aur.archlinux.org/ttf-wps-fonts.git
+# cd ttf-wps-fonts
+# makepkg
+# sudo pacman -U *.zst
+# cd ..
+# rm -r ttf-wps-fonts
+
+# # 如果需要中文化，注意只需要安装wps-office-mui-zh-cn
+# git clone https://aur.archlinux.org/wps-office-cn.git
+# cd wps-office-cn
+# makepkg
+# sudo pacman -U wps-office-mui-zh-cn*.zst
+# cd ..
+# rm -r wps-office-cn
